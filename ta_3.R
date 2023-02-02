537ef287dc625331978071e75eb8fe7f42825536
Jeremy <jjeremy1@vt.edu>
2023-02-02 13:21:34 -0500

    new commit

--- ta_3.R
+++ ta_3.R
@@ -7,14 +7,17 @@ library(rfishbase)
 lm1 = lm(log(HerringBWE$ssb)~log(HerringBWE$spawning.year))
 ggplot() + geom_point(data=HerringBWE, aes(x=log(spawning.year), y=log(ssb))) + geom_line(data=HerringBWE, aes(x=log(spawning.year), y=lm1$fitted.values))
 plot(lm1)
+lm2 = lm(data=HerringBWE, log(ssb/recruits)~spawning.year)
+ggplot() + geom_point(data=HerringBWE, aes(x=log(spawning.year), y=log(ssb/recruits))) + geom_line(data=HerringBWE, aes(x=log(spawning.year), y=lm2$fitted.values))
+plot(lm2)
 
 ## Black Drum
 BlackDrum2001 = na.omit(BlackDrum2001)
-lm2 = lm(log(BlackDrum2001$weight)~log(BlackDrum2001$tl))
+lm3 = lm(log(BlackDrum2001$weight)~log(BlackDrum2001$tl))
 model_bd = nls(weight~a*tl^b, start=list(a=160,b=3), data=BlackDrum2001)
 coef(model_bd)
 p1 = ggplot() + geom_point(data=BlackDrum2001, aes(x=tl, y=weight)) + geom_line(data=BlackDrum2001, aes(x=tl, y=predict(model_bd)))
-p2 = ggplot() + geom_point(data=BlackDrum2001, aes(x=log(tl), y=log(weight))) + geom_line(data=BlackDrum2001, aes(x=log(tl), y=lm2$fitted.values))
+p2 = ggplot() + geom_point(data=BlackDrum2001, aes(x=log(tl), y=log(weight))) + geom_line(data=BlackDrum2001, aes(x=log(tl), y=lm3$fitted.values))
 plot(model_bd)
 gridExtra::grid.arrange(p1, p2)
 
@@ -39,6 +42,29 @@ t0 = params[3]
 La = Linf*(1-exp(-K*(1.5-t0)))
 La[[1]]
 
+## Q2
+b = c(0.15,0.2,0.25) # birth rates
+d = c(0.07,0.07,0.07) # death rates
+N0 = c(1500,1500,1500) # starting population number
+Time = seq(0,30,2) # time steps
+Nt = matrix(NA, nrow = length(Time), ncol = 3)
+for (i in 1:3){
+  Nt[,i] = N0[i]*exp((b[i]-d[i])*Time)
+}
+plot(Nt[,1]~Time, type = "l", ylim = range(Nt))
+lines(Nt[,2]~Time, col = "red")
+lines(Nt[,3]~Time, col = "blue")
 
+
+## Q3
+hadLife = subset(popgrowth(species_list = "Melanogrammus aeglefinus"), Locality=="North Sea")
+hadMat = subset(maturity(species_list = "Melanogrammus aeglefinus"), Locality=="North Sea")
+hadFec = subset(fecundity(species_list = "Melanogrammus aeglefinus"), Locality=="North Sea")
+
+lifespan = mean(hadLife$tmax, na.rm=T)
+agemat = sum(c(hadMat$AgeMatMin,hadMat$AgeMatMin2), na.rm=T) / 4
+fecundity = hadFec$FecundityMax[2]
+
+rmax = getRmax(method = "Smith.etal.1998", mortality = "Then.etal.2014", fec = fecundity, lifespan=lifespan, agemat=agemat)
 
 
